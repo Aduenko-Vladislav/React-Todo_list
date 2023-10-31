6371c2a93c451b35f0c842959eb8edc704ec2664
Vladislav <adyenko.job@gmail.com>
2023-10-31 15:56:19 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+  # Project React to Do List
+<div align="center">
+    <p align="center">
+    *
+    <a href="#">Watch Demo</a>
+    * </p>
+</div>
+
---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>Hello world its my to-do List</h1>
+    </div>
+  );
+}
+
+export default App;
